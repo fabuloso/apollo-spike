668b5a490d651f834b83d4634bb785f4b8f50751
Alessandro Dalfovo <dalfovo.alessandro@paros.srl>
2016-05-24 13:55:52 +0200

    Add docker and mongo repository

--- src/main/java/Apollo.java
+++ src/main/java/Apollo.java
@@ -1,17 +1,41 @@
 import com.spotify.apollo.Environment;
+import com.spotify.apollo.RequestContext;
+import com.spotify.apollo.Response;
 import com.spotify.apollo.httpservice.HttpService;
 import com.spotify.apollo.httpservice.LoadingException;
 import com.spotify.apollo.route.Route;
+import okio.ByteString;
 
 public class Apollo {
 
+    private static MongoDbRepository repository;
+
     public static void main(String[] args) throws LoadingException {
         HttpService.boot(Apollo::init, "apollo", args);
+        //repository = new MongoDbRepository();
+        //seed(repository);
     }
 
     static void init(Environment environment) {
         environment.routingEngine()
-                .registerAutoRoute(Route.sync("GET", "/", context -> "hello world"))
+                .registerAutoRoute(Route.sync("GET", "/", Apollo::doSomething))
                 .registerAutoRoute(Route.sync("GET", "/hello", context -> "bye"));
+    }
+
+    private static Response<ByteString> doSomething(RequestContext requestContext) {
+        //repository.findAll();
+        return Response.ok().withPayload(ByteString.encodeUtf8("pong!"));
+    }
+
+    private static void seed(MongoDbRepository repository) {
+        repository.drop();
+        for (int i = 0; i < 10; i++) {
+            String json = jsonFor(i);
+            repository.insert(json);
+        }
+    }
+
+    private static String jsonFor(int i) {
+        return "{\"foo\":\"1\",\"bar\":{\"baz\":\"" + i + "\"}}";
     }
 }
