861bc5892c501bc7a57669222131022cbf54c19d
Alessandro Dalfovo <alessandro.dalfovo@paros.srl>
2017-06-06 14:10:44 +0200

    Add view project on docker compose

--- src/main/java/domain/event/BookLentBack.java
+++ src/main/java/domain/event/BookLentBack.java
@@ -5,8 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BookLentBack implements Event{
-    private String title;
     final static Logger LOG = LoggerFactory.getLogger(BookLentBack.class);
+
+    private String title;
 
     public BookLentBack(String title) {
         this.title = title;
