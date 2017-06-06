18e8c621abf9fdc930e512a34113b145e4440391
Alessandro Dalfovo <alessandro.dalfovo@paros.srl>
2017-06-06 14:09:36 +0200

    Removed MongoDb as repo

--- src/main/java/Apollo.java
+++ src/main/java/Apollo.java
@@ -5,8 +5,8 @@ import com.spotify.apollo.httpservice.HttpService;
 import com.spotify.apollo.httpservice.LoadingException;
 import command.AddBook;
 import command.CommandHandler;
-import command.LendBook;
 import command.LendBackBook;
+import command.LendBook;
 import okio.ByteString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,32 +17,29 @@ import static command.Commands.addBookCommandFrom;
 
 public class Apollo {
 
-    private static MongoDbRepository repository;
     private static CommandHandler commandHandler;
 
     final static Logger LOG = LoggerFactory.getLogger(Apollo.class);
 
 
     public static void main(String[] args) throws LoadingException {
+        LOG.info("INIT COMMAND HANDLERS");
         commandHandler = new CommandHandler();
-        LOG.info("INIT MONGO");
-        repository = new MongoDbRepository();
-        LOG.info("SEED MONGO");
-        seed(repository);
+
         LOG.info("INIT ROUTES");
         HttpService.boot(Apollo::init, "apollo", args);
     }
 
     static void init(Environment environment) {
         environment.routingEngine()
-                .registerAutoRoute(sync("GET", "/addBook", Apollo::addBook))
+                .registerAutoRoute(sync("GET", "/addBook",Apollo::addBook))
                 .registerAutoRoute(sync("GET", "/lendBook", Apollo::lendBook))
                 .registerAutoRoute(sync("GET", "/lendBackBook", Apollo::lendBackBook));
     }
 
     private static Response<ByteString> lendBook(RequestContext requestContext) {
-        String name = requestContext.request().parameter("name").get();
         String title = requestContext.request().parameter("title").get();
+        String name = requestContext.request().parameter("name").get();
 
         LendBook command = new LendBook(title, name);
 
@@ -68,17 +65,5 @@ public class Apollo {
         commandHandler.handle(command);
 
         return ok();
-    }
-
-    private static void seed(MongoDbRepository repository) {
-        repository.drop();
-        for (int i = 0; i < 10; i++) {
-            String json = jsonFor(i);
-            repository.insert(json);
-        }
-    }
-
-    private static String jsonFor(int i) {
-        return "{\"title\":\"a title\",\"author\":{\"name\":\"" + "name " + i + "\"}}";
     }
 }
